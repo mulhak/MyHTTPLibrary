fea7f74ea142b5da2332fc5f794f2d7e2de22249
mulhak <mulhakieran@gmail.com>
2021-04-30 17:25:49 +0100

    GetUsers to add to localstorage

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('get').addEventListener('click', getUsers);
-document.getElementById('post').addEventListener('click', createPost);
+document.getElementById('post').addEventListener('click', createUser);
 document.getElementById('update').addEventListener('click', updateUser);
 document.getElementById('delete').addEventListener('click', deleteUser);
 
@@ -8,38 +8,46 @@ const http = new MyHTTP;
 function getUsers(){
  http.get('https://jsonplaceholder.typicode.com/users')
  .then(data =>{
+     let outputLocal = '';
      let output = ''; 
      data.forEach(function(user){
+     outputLocal +=`${user.name}, ${user.username}`;
      output += `<li>${user.name}, ${user.username}</li>`;
      });
-     document.getElementById('output').innerHTML = output;
- })
- .catch(err => console.log(err));}
+     localStorage.setItem('outputLocal', JSON.stringify(outputLocal));
+     document.getElementById('output').innerHTML = output;     
+    })
+      .catch(err => console.log(err));}
 
 
 //User Data for post,put and delete..
-const data = {
+ const data = {
    name: 'Kieran Mulhall',
-   username:'Jackdaw',
-   email: 'kieran@gmail.com'
-}
+   username:'Jackdaw'
+ }
 
-function createPost(){
-http.post('https://jsonplaceholder.typicode.com/users', data)
-.then(data =>console.log(data)) 
-.catch(err => console.log(err));}
+ function createUser(){
+    http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data =>{
+        localStorage.getItem(outputLocal);
+        document.getElementById('outputLocal').innerHTML =data 
+        // incorrect above as this is going to jsonplaceholder......
+
+
+    }) 
+    .catch(err => console.log(err));}
 
 
 
 
  function updateUser(){
- http.put('https://jsonplaceholder.typicode.com/users/2', data)
- .then(data =>console.log(data))
- .catch(err => console.log(err));}
+    http.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data =>console.log(data))
+    .catch(err => console.log(err));}
 
 
  function deleteUser() {
- http.delete('https://jsonplaceholder.typicode.com/users/2')
- .then(data =>console.log(data))
- .catch(err => console.log(err));}
- 
+    http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data =>console.log(data))
+    .catch(err => console.log(err));}
+    
